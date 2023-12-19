131e055f00d6b4f137e795fe2bc618fba4dcf183
Smit Patel <smitvtridhyatech@gmail.com>
2023-12-19 11:09:30 +0530

    resolved conflict of auth branch

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,8 @@ const paymentRoute = require("./paymentRoute");
 
 router.use("/api/v1/user", userRoute);
 router.use("/api/v1/profile", profileRoute);
-<<<<<<< HEAD
 router.use("/api/v1/payment", paymentRoute);
-=======
 router.use("/api/v1/role", roleRoute);
 router.use("/api/v1/track", trackRoute);
->>>>>>> 183628d8736aa7c7e7e0446e9c259a00de138465
 
 module.exports = router;
