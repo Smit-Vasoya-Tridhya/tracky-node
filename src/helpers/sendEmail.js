96e35e8b39da014ec31b0b4713eeba52d8421990
Keyur Modi <keyurmtridhyatech@gmail.com>
2024-01-10 17:30:30 +0530

    test mail

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const sendEmail = async (email, message, subject) => {
+  console.log("Mail", process.env.MAI);
+  console.log("Mail PAssword", process.env.MAILPASSWORD);
   const transporter = nodemailer.createTransport({
     port: 465,
     service: "gmail",
